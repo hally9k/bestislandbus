ad4502b8a6a8aaa67f0ae6524c8d12fcd6eb7138
Hal Smith Stevens <hally9k@gmail.com>
2023-01-03 19:43:38 +1300

    ...

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,6 +12,12 @@ const config: GatsbyConfig = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/logo.png',
+      },
+    },
   ],
 }
 
